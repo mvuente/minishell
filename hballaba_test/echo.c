64d3666cbadd46b1445e7db950374acd38e89775
Howard Ballabar <hballaba@si-k2.kzn.21-school.ru>
2020-11-12 17:28:04 +0300

    all

--- hballaba_test/echo.c
+++ hballaba_test/echo.c
@@ -6,50 +6,79 @@
 #include <stdlib.h>
 
 
-void ft_echo(char *name, char *line, int option)
+void ft_echo_doubledir(char *name, char *line, int option)
+{
+     int fd;
+
+    fd = 1;
+    if (name)
+       fd = open(name, O_RDWR | O_CREAT | O_APPEND, S_IRWXU);
+    if (line)
+    {
+        write(fd, line, strlen(line));
+        free(line);
+    }
+    if (option == 0)
+       write(fd, "\n", 1);     
+    free(name);
+}
+
+void ft_echo_dir(char *name, char *line, int option)
 {
     int fd;
+
+    fd = 1;
     if (name)
+       fd = open(name, O_RDWR | O_CREAT | O_TRUNC, S_IRWXU);
+    if (line)
     {
-       // printf("%s\n", name);
-        //printf("%s\n", line);
-       
-        fd = open(name, O_RDWR | O_CREAT | O_APPEND, S_IRWXU);
-        if (line)
-           {
-               write(fd, line, strlen(line));
-               free(line);
-           }
-        if (option == 0)
-            write(1, "\n", 1);
-        close(fd);
-        free(name);
+        write(fd, line, strlen(line));
+        free(line);
     }
-    else
+    if (option == 0)
+       write(fd, "\n", 1);     
+    free(name);
+}
+
+void ft_echo_backdir(char *name, char *line, int option)
+{
+    int oldfd;
+    int newfd;
+
+    oldfd = open(name, O_RDONLY);
+    if (oldfd == -1)
     {
-        if (line)
-            {
-                write(1, line, strlen(line));
-                free(line);
-            }
-        if (option == 0)
-            write(1, "\n", 1);
-        
+        write(1, name, strlen(name));
+        write(1, ": No such file or directory\n", 28);
+        return ;
     }
+    dup2(oldfd, newfd);
+
+    close(oldfd);
 
 }
 
+void ft_echo(char *name, char *line, int option, char direct)
+{
+    if (direct == '^')
+        ft_echo_doubledir(name, line, option);
+    if (direct == '>')
+        ft_echo_dir(name, line, option);
+    if (direct == '<')
+        ft_echo_backdir(name, line, option);
+}
 
 
 int main(int argc, char **argv)
 {
-        char *name = strdup("test");
+        char *name = strdup("test1");
         char *line = strdup("hello");
         int option = 0;
-        char *lin1 = NULL;
+        char *line1 = NULL;
         char *name1 = NULL;
-        ft_echo(name1, line, 0);
+
+        int direct = 0;
+        ft_echo(name, line, 0, '<');
     
     return (0);
-
 }
---
+++ hballaba_test/pipe.c
@@ -0,0 +1,41 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int
+main(int argc, char *argv[])
+{
+    int pipefd[2];
+    pid_t cpid;
+    char buf;
+    char *reads = "hello";
+   /* if (argc != 2) {
+        fprintf(stderr, "Использование: %s <string>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }*/
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    cpid = fork();
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (cpid == 0) {    /* Потомок читает из канала */
+        close(pipefd[1]);          /* Закрывает неиспользуемый конец для записи */
+        while (read(pipefd[0], &buf, 1) > 0)
+            write(1, &buf, 1);
+        write(1, "\n", 1);
+        close(pipefd[0]);
+        _exit(EXIT_SUCCESS);
+    } else {            /* Родитель пишет значение argv[1] в канал */
+        close(pipefd[0]);          /* Закрывает неиспользуемый конец для чтения */
+        write(pipefd[1], reads, strlen(reads));
+        close(pipefd[1]);          /* Читатель видит EOF */
+        wait(NULL);                /* Ожидание потомка */
+        exit(EXIT_SUCCESS);
+    }
+}

39dbc226d39197e5bb9c984660243c51ae575c66
Howard Ballabar <hballaba@si-k2.kzn.21-school.ru>
2020-11-19 17:02:38 +0300

    unset

---
+++ hballaba_test/unset.c
@@ -0,0 +1,78 @@
+  #include "minishell.h"
+
+
+
+
+void (ft_no_valid_word_unset(char *word))
+{
+    ft_putstr_fd("bash: unset: `", 1);
+    ft_putstr_fd(word, 1);
+    ft_putstr_fd("': not a valid identifier\n", 1);
+}
+
+int ft_delete_env(char *str, t_env	*bufenv)
+{
+    int i;
+    int flag;
+    t_env *tmp;
+    void *ptr;
+    
+    i = ft_strlen(str);
+    flag =0;
+    while (bufenv->next)
+    {
+        if(ft_strncmp(bufenv->name, str, i) == 0 && i == ft_strlen(bufenv->name))
+        {
+            flag = 1;
+            tmp->next = bufenv->next;
+            free(bufenv->data);
+            free(bufenv->name);
+            free(bufenv);
+        }
+        if (flag == 1)
+            break ;
+       tmp = bufenv;
+       bufenv = bufenv->next;
+    }
+    if (flag == 0)
+        return (1);
+    return (0);
+}
+
+
+int ft_check_word_unset(char *word)
+{
+    int i;
+    char c;
+    int flag;
+
+    flag = 0;
+    i = -1;
+    if (word[0] >= '0' && word[0] <= '9')
+        return (0);
+    while (word[++i])
+    {
+        c = word[i];
+        
+        if ((c >= '*' && c <= '/') || (c >= ':' && c <= '@') || (c >= '{' && c <= '~'))
+            return (1);
+    return (0);
+}
+
+
+void	unset_executer(t_set *set, int **fd, t_all *all)
+{
+    int check;
+    
+    while(set->word)
+       {
+           check = ft_check_word_enset(set->word->word);
+           if (check == 1)
+               ft_no_valid_word_unset(set->word->word);
+           else 
+               ft_delete_env(set->word->word, all->myenv);
+           set->word = set->word->next;
+        }
+    }
+}
+

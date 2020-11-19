83f2a8b11b6377375780cca9416b77bd9b8174df
Howard Ballabar <hballaba@si-k2.kzn.21-school.ru>
2020-11-19 19:17:30 +0300

    unset

--- hballaba_test/unset.c
+++ hballaba_test/unset.c
@@ -56,23 +56,55 @@ int ft_check_word_unset(char *word)
         
         if ((c >= '*' && c <= '/') || (c >= ':' && c <= '@') || (c >= '{' && c <= '~'))
             return (1);
+     
     return (0);
 }
 
+int ft_check_258(t_set *set, t_all *all)
+{
+    while(set->word)
+       {
+            if (ft_strncmp(word->word, '('))
+            {
+               ft_putendl_fd("bash: syntax error near unexpected token `('", fd);
+               return (0);
+            }
+            if (ft_strncmp(word->word, ')'))
+            {
+               ft_putendl_fd("bash: syntax error near unexpected token `)'", fd);
+               all->error = 258;
+               return (0);
+            }
+            set->word = set->word->next;
+       }
+       return (1);
+}
 
-void	unset_executer(t_set *set, int **fd, t_all *all)
+
+int unset_executer(t_set *set, int **fd, t_all *all)
 {
     int check;
+    int flag;
+
+    flag = 0;
+    if (!ft_check_258(set, all))
+        return (258);
+
     
     while(set->word)
        {
            check = ft_check_word_enset(set->word->word);
            if (check == 1)
-               ft_no_valid_word_unset(set->word->word);
+               {
+                ft_no_valid_word_unset(set->word->word);
+                all->error = 1;
+                flag = 1;
+               }
            else 
                ft_delete_env(set->word->word, all->myenv);
            set->word = set->word->next;
         }
-    }
+       if (flag == 1)
+        return (1);
+    return (0);
 }
-

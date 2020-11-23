2b47ecec370f438412109fc022afaf9c7b54127d
Howard Ballabar <hballaba@si-k2.kzn.21-school.ru>
2020-11-23 19:01:56 +0300

    pipe

--- working_version/pipe_exec.c
+++ working_version/pipe_exec.c
@@ -36,66 +36,72 @@ int **ft_init_pipefd(int size)
 
 
 
-int ft_work_pipe(t_all *all, t_genlist *pipes, int size, int **pipefd, int **fd)
+int ft_work_pipe(t_all *all, t_genlist *pipes, int size, int pipefd[2], int **fd)
 {
     pid_t cpid;
     int status;
     int flag;
-
+   
     flag = 0;     
      if ((cpid = fork())== -1)
         return(1);
-        
+        //ft_putendl_fd("hello", 1);
         if (cpid == 0) 
         { // Потомок читает из канала 0 
-           close(pipefd[size][0]); 
+           close(pipefd[0]); 
            //if (flag == 0)
-            dup2(pipefd[size][1], 1); 
+            dup2(pipefd[1], 1); 
            //else 
              //  dup2(pipefd[size][1], pipefd[size+1][1]); 
             flag = 1;
-           
-           ft_exe_function(pipes, all, fd); // ту вызывается функция которая вызовет или системную или самописную
-        } 
+           ft_exe_function(pipes, all, fd);
+           exit(0); /// тут надо вернуть status после системной команды
+            
+           } 
         else 
         {   
-            close(pipefd[size][1]);         
-            dup2(pipefd[size][0], 0);
-            waitpid(cpid, &status, WUNTRACED);
-           //printf("%d\n", size);
+           close(pipefd[1]);         
+           dup2(pipefd[0], 0); 
+
+              waitpid(cpid, &status, WUNTRACED); //WNOHANG   WUNTRACED
+          
+          
            if (size < 1) // значит что это последня команда и надо вернуть fd в первоначально состояние, чтобы ушло в стдаут терминал
-              {
+           {
                     dup2(all->fd_1, 1);
-            }
-           // else 
-             //  dup2(pipefd[size-1][1], pipefd[size][0]);
-            pipes = pipes->next;
-            if (size < 1)
-               {
-                    ft_exe_function(pipes, all, fd);
-                     dup2(all->fd_0, 0);
-               } 
+                   
+           }
+           pipes = pipes->next;
+           if (size < 1)
+           {
+                ft_exe_function(pipes, all, fd);
+                dup2(all->fd_0, 0);
+                
+           }
         }
+     
     return (0);
 }
 // ft_pipe(&all, pipes, (ft_lstsize_pipes(pipes)) - 1);
 
 int ft_pipe(t_all *all, t_genlist *pipes, int size, int **fd)
 {
-    int **pipefd;
+    int pipefd[2];
     pid_t cpid;
     int flag;
     int status;
      
-    if (!(pipefd = ft_init_pipefd(--size)))
-        return (1);
+    size--;
     while (size >= 0)
     {
-            printf("size = %d\n", size);
+            if (pipe(pipefd) == -1)
+                retutn (0);
+            //printf("built  %s\n", pipes->set->builtin);
             ft_work_pipe(all, pipes, size, pipefd, fd);
-        
+         
         size--;
         pipes = pipes->next;
+        //printf("built  %s\n", pipes->set->builtin);
     }
    return (0);
 }

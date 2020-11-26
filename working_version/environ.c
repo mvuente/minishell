90014ac9b8902a6098d63b50cb5d704091c24f8b
Howard Ballabar <hballaba@si-k6.kzn.21-school.ru>
2020-11-26 15:49:12 +0300

    environ

--- working_version/environ.c
+++ working_version/environ.c
@@ -1,12 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environ.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hballaba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 15:47:21 by hballaba          #+#    #+#             */
+/*   Updated: 2020/11/26 15:47:42 by hballaba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 char *ft_get_value(t_env *myenv, char *name)
 {
-    while (myenv)
-        {
-            if (!ft_memcmp(myenv->name, name, ft_strlen(myenv->name + 1)))
-                return (myenv->data);
-            myenv = myenv->next;
-        }
-    return(NULL);
+   int i;
+
+	i = ft_strlen(name);
+	while (myenv)
+	{
+		if (!ft_memcmp(myenv->name, name, i + 1)  && i == ft_strlen(myenv->name))
+				return (myenv->data);
+		myenv = myenv->next;
+	}
+	return (NULL);
 }

5c18495f709f589d9170c0471c81f1ca47028ceb
Howard Ballabar <hballaba@si-n6.kzn.21-school.ru>
2020-11-27 20:30:41 +0300

    minishell gnl

--- working_version/minishell.c
+++ working_version/minishell.c
@@ -4,11 +4,30 @@ int     minishell(t_all all)
 {
     size_t  ret;
 	char	*line;
+    char buf[1];
 
-    while ((ret = gnl(0, &line)) > 0)
+    while ((ret = gnl(0, &line)) >= 0)
     {
-        reader(line, all);// line came back cause of \n
-		free(line);
+        
+        //printf("%zu\n", ret);
+        if (ret == 0)
+        {
+            
+             if (!line[0])
+            {
+                
+                ft_putendl_fd("exit", 1);
+                exit(0);
+            }
+            while ((ret = read(0, buf, 1)) == 0)
+            {
+                
+            }
+        }  
+          
+            reader(line, all);// line came back cause of \n
+		    free(line);
+        
     }
     if (ret == 0)// (it's if CTRL+D push)
         exit(0);

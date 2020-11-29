dcbde9c93a26ffb11207da199f04c987e1f760f1
Howard Ballabar <hballaba@si-k6.kzn.21-school.ru>
2020-11-29 13:51:01 +0300

    pwd

--- working_version/pwd.c
+++ working_version/pwd.c
@@ -4,13 +4,10 @@ void ft_pwd(t_all *all)
 {
    char dir[500];
    int	descr;
-   
-    getcwd(dir, 500);
-    write(1, dir, ft_strlen(dir));
-    dup2(1, descr);
-    if (descr != 1)
-    	close(descr);
-    dup2(all->fd_1, 1);
-    write(1, "\n", 1);
-    errno = 0;
+   getcwd(dir, 500);
+   ft_putendl_fd(dir, 1);
+   dup2(1, descr);
+   close(descr);
+   dup2(all->fd_1, 1);
+   errno = 0;
 }
